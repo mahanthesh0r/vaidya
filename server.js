336feb550ce05d3b94132e7bdfa5f023ec76500f
Mahanthesh <monty.officio@gmail.com>
2024-03-30 14:59:43 -0400

    illness API added

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ app.use('/assets',express.static('assets'));
 app.use(express.urlencoded({ extended: true }));
 
 let api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImNvb2xtb250ZWVlQGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMTM3NDUiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMjQtMDMtMjkiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTcxMTgzMTExOSwibmJmIjoxNzExODIzOTE5fQ.yNcBeHxHetl-YBertzz3cV_W9eDmgECHoQnObnXLtDU"
-let API = "https://sandbox-healthservice.priaid.ch/symptoms?token=" + api_key + "&format=json&language=en-gb"
+let BASE_URL = "https://sandbox-healthservice.priaid.ch"
+let format = "&format=json"
+let language = "&language=en-gb"
 let formData = {};
 let symptomsList = [];
+let illnessList = [];
 
 app.get("/", (req, res) => {
     res.render('index', { step: 1, formData });
@@ -26,9 +29,18 @@ app.post('/next', async (req, res) => {
     if(nextStep === 2) {
         if (formData.symptoms){
             try {
-                const response = await axios.get(API);
+                const response = await axios.get(BASE_URL + "/symptoms?token=" + api_key + format + language);
                 // Passing fetched data to EJS template
                 symptomsList = response.data
+              } catch (error) {
+                console.error('API call error: ', error);
+                res.send('Error fetching data');
+              }
+        } else if(formData.illness) {
+            try {
+                const response = await axios.get(BASE_URL + "/issues?token=" + api_key + format + language);
+                // Passing fetched data to EJS template
+                illnessList = response.data
               } catch (error) {
                 console.error('API call error: ', error);
                 res.send('Error fetching data');
@@ -44,7 +56,13 @@ app.post('/next', async (req, res) => {
         return res.send('Form Submitted'); 
     }
 
+    if(symptomsList.length != 0)
      res.render('index', { step: nextStep, formData, data: symptomsList });
+    else if(illnessList != 0)
+    res.render('index', { step: nextStep, formData, data: illnessList });
+    else
+    res.render('index', { step: nextStep, formData});
+
     
    
 });
