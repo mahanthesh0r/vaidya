8bbda3a0b2573b13a42f7721abfd30a0d18f0587
Mahanthesh <monty.officio@gmail.com>
2024-03-31 18:32:00 -0400

    bug fixes

--- server.js
+++ server.js
@@ -2,20 +2,23 @@ const express = require('express')
 const app = express()
 const path = require("path");
 const axios = require('axios')
+const dotenv = require('dotenv');
 const NodeCache = require('node-cache');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use('/assets',express.static('assets'));
 app.use(express.urlencoded({ extended: true }));
+dotenv.config();
 
-let api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImNvb2xtb250ZWVlQGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMTM3NDUiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMjQtMDMtMjkiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTcxMTkyNjQ5MCwibmJmIjoxNzExOTE5MjkwfQ.wk1W0Yd-VkbQqwyhxXPE22zWFuppQOeUZBn9RpJQjjQ"
-let BASE_URL = "https://sandbox-healthservice.priaid.ch"
+let api_key = process.env.API_KEY;
+let BASE_URL = process.env.BASE_URL;
 let format = "&format=json"
 let language = "&language=en-gb"
 let formData = {};
 let symptomsList = [];
 let illnessList = [];
 const myCache = new NodeCache();
+
 
 app.get("/", (req, res) => {
     res.render('index', { step: 1, formData });
@@ -117,7 +120,7 @@ try {
   
 })
 
-const port = 3001
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
